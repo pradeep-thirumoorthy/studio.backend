598946eb553d2ab4e6613286ab02478acdcccefc
LernMaster <121391566+LernMaster@users.noreply.github.com>
2024-05-14 13:03:33 +0530

    third commit

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use('/user', userRoutes);
 app.use('/admin', adminRoutes);
 
