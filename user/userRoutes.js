3efdb3ddb71311fcc3d6069102652e04d74dd9d1
LernMaster <121391566+LernMaster@users.noreply.github.com>
2024-05-14 19:48:44 +0530

    host-integeration

--- user/userRoutes.js
+++ user/userRoutes.js
@@ -19,7 +19,7 @@ router.post('/login', async (req, res) => {
     }
     const secretKey = 'your_secret_key';
     const token = jwt.sign({ username: user.username }, secretKey, { expiresIn: '1h' });
-    res.cookie('token', token, { httpOnly: true, maxAge: 3600000}); // Expires in 1 hour
+    res.cookie('token', token, { httpOnly: true, maxAge: 3600000, sameSite: 'None'}); // Expires in 1 hour
 
     return res.status(200).json({ success: true, message: 'Login successful', token });
   } catch (err) {
