403c1fd3a5c9c882e955512d05af4c1bb27c86bb
LernMaster <121391566+LernMaster@users.noreply.github.com>
2024-05-14 16:21:55 +0530

    vercel2

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ const express = require('express');
 // const cors = require('cors');
 // const userRoutes = require('./user/userRoutes.js');
 // const adminRoutes = require('./admin/adminRoutes.js');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const { Service } = require('./schema.js');
 
-// const uri = process.env.MONGODB_URI;
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 
-// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-// async function run() {
-//   try {
-//     await mongoose.connect(uri, clientOptions);
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
     
-//   }
-// }
-// run().catch(console.dir);
+  }
+}
+run().catch(console.dir);
 const app = express();
 const port = process.env.PORT;
 
 // Middleware
-app.use(cors(
-//   {
-//   origin: '',
-//   credentials: true
-// }
-));
+// app.use(cors(
+// //   {
+// //   origin: '',
+// //   credentials: true
+// // }
+// ));
 // app.use(bodyParser.json({ limit: '500mb' }));
 // app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
 
