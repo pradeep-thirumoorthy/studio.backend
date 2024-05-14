e4dd5cba26410a6bd891763bf15292f120dfc648
LernMaster <121391566+LernMaster@users.noreply.github.com>
2024-05-14 16:11:20 +0530

    vercel2

--- index.js
+++ index.js
@@ -47,15 +47,15 @@ app.get("/", (req, res) => {
 //   res.status(500).send('Internal Server Error');
 // });
 
-// app.get('/services', async (req, res) => {
-//   try {
-//     //const services = await Service.find();
-//     res.status(200).json("services");
-//   } catch (error) {
-//     console.error('Error fetching services:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+app.get('/services', async (req, res) => {
+  try {
+    //const services = await Service.find();
+    res.status(200).json({"services":"qawsedrf"});
+  } catch (error) {
+    console.error('Error fetching services:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 // module.exports = db;
