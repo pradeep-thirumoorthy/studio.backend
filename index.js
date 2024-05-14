82d1873e4f8dbe1cbc5cbd1d788f15423f5066ee
LernMaster <121391566+LernMaster@users.noreply.github.com>
2024-05-14 16:13:33 +0530

    vercel2

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ const app = express();
 const port = process.env.PORT;
 
 // Middleware
-// app.use(cors(
-// //   {
-// //   origin: '',
-// //   credentials: true
-// // }
-// ));
-// app.use(bodyParser.json({ limit: '500mb' }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
+app.use(cors(
+//   {
+//   origin: '',
+//   credentials: true
+// }
+));
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
