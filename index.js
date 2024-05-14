5aecfbc69be6e6168dfc1186ab72f1f59d700f17
LernMaster <121391566+LernMaster@users.noreply.github.com>
2024-05-14 16:07:12 +0530

    vercel2

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const adminRoutes = require('./admin/adminRoutes.js');
 const mongoose = require('mongoose');
 const { Service } = require('./schema.js');
 
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
+// const uri = process.env.MONGODB_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
-  try {
-    await mongoose.connect(uri, clientOptions);
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+// async function run() {
+//   try {
+//     await mongoose.connect(uri, clientOptions);
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
     
-  }
-}
-run().catch(console.dir);
+//   }
+// }
+// run().catch(console.dir);
 const app = express();
 const port = process.env.PORT;
 
@@ -39,8 +39,8 @@ app.use(cors(
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-app.use('/user', userRoutes);
-app.use('/admin', adminRoutes);
+// app.use('/user', userRoutes);
+// app.use('/admin', adminRoutes);
 
 // app.use((err, req, res, next) => {
 //   console.error(err.stack);
@@ -49,8 +49,8 @@ app.use('/admin', adminRoutes);
 
 app.get('/services', async (req, res) => {
   try {
-    const services = await Service.find();
-    res.status(200).json(services);
+    //const services = await Service.find();
+    res.status(200).json("services");
   } catch (error) {
     console.error('Error fetching services:', error);
     res.status(500).json({ error: 'Internal Server Error' });
